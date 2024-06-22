15717f0ef1d994bac8eafd330188dd5de808ac92
王旭 <wxu743571@gmail.com>
2024-06-22 21:00:37 +0800

    提取skeleton组件

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,11 +1,10 @@
+import Skeleton from "@/app/components/Skeleton";
 import { Box, Card, Flex } from "@radix-ui/themes";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
   return (
     <Box className="max-w-xl">
-      <Skeleton/>
+      <Skeleton />
       <Flex className="space-x-3" my="2">
         <Skeleton width="5rem" />
         <Skeleton width="8rem" />
