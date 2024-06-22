a4da98e6ecb07252403b347c789712686c871668
王旭 <wxu743571@gmail.com>
2024-06-22 19:00:47 +0800

    显示问题

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -25,8 +25,8 @@ const NavBar = () => {
               href={link.href}
               className={classNames({
                 "text-zinc-900": currentPath === link.href,
-                "text-zinc-400": currentPath !== link.href,
-                "text-zinc-800 transition-colors": true,
+                "text-zinc-500": currentPath !== link.href,
+                "hover:text-zinc-800 transition-colors": true,
               })}
             >
               {link.label}
