967c1927fc64bcd7f1f13c8df260286238a60ed5
王旭 <wxu743571@gmail.com>
2024-06-23 20:33:20 +0800

    过滤问题

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,21 +1,32 @@
+"use client";
+
 import { Select } from "@radix-ui/themes";
 import React from "react";
 import { Status } from "@prisma/client";
+import { useRouter } from "next/navigation";
 
 const IssueStatusFilter = () => {
-  const statuses: { label: string; value?: Status | "all" }[] = [
+  const statuses: { label: string; value: Status | "all" }[] = [
     { label: "All", value: "all" },
     { label: "Open", value: "OPEN" },
     { label: "In progress", value: "IN_PROGRESS" },
     { label: "Closed", value: "CLOSED" },
   ];
+
+  const router = useRouter();
+
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(status) => {
+        const option = status === "all" ? "" : "?status=" + status;
+        router.push("/issues/list" + option);
+      }}
+    >
       <Select.Trigger placeholder="Filter by Status..." />
       <Select.Content>
         <Select.Group>
           {statuses.map((status) => (
-            <Select.Item key={status.value} value={status.value || ""}>
+            <Select.Item key={status.value} value={status.value}>
               {status.label}
             </Select.Item>
           ))}
