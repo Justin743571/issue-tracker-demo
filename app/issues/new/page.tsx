7bf85b687896a50503688341ff15c7ac1d895bd7
王旭 <wxu743571@gmail.com>
2024-06-24 13:37:25 +0800

    修复/issues/new页面的渲染

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import dynamic from "next/dynamic";
+import IssueFormSkeleton from "./loading";
 
-const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"));
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 const NewIssuePage = () => {
   return <IssueForm />;
